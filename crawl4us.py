75618e69d06fd1556ee978488a7f19af32cf1063
Camila Maia <cmaiacd@gmail.com>
2018-03-18 21:08:56 -0300

    Add index and whole_page params to crawl

--- crawl4us.py
+++ crawl4us.py
@@ -1,5 +1,12 @@
 import requests
+import logging
+import time
+
 from bs4 import BeautifulSoup
+import pandas as pd
+
+
+log = logging.getLogger(__name__)
 
 
 def _load_page(url):
@@ -9,29 +16,27 @@ def _load_page(url):
     return page
 
 
-def _wtih_header(table):
-    header = table[0]
-    header = [h.lower() for h in header]
-    return (table, header)
+def _find_table(page, index, whole_page):
+    if whole_page:
+        return page
+
+    return page.find_all('table')[index]
 
 
-def _clean_table(table):
-    table.pop(0)
-
-    return table
-
-
-def _scrap_table(page):
+def _crawl_table(html_table):
     table = []
 
-    for tr in page.find_all('tr'):
+    for tr in html_table.find_all('tr'):
         row = []
+        for th in tr.find_all('th'):
+            row.append(th.text.strip())
+
         for td in tr.find_all('td'):
             row.append(td.text.strip())
 
         table.append(row)
 
-    return _clean_table(table)
+    return table
 
 
 def _map_columns_names(col, official_header):
@@ -41,11 +46,24 @@ def _map_columns_names(col, official_header):
     return '--'
 
 
-def crawl(url, official_header):
-    page = _load_page(url)
-    table = _scrap_table(page)
-    table, header = _wtih_header(table)
-    header = [_map_columns_names(h, official_header) for h in header]
+def _parse_header(table, official_header):
+    header = table[0]
+    header = [_map_columns_names(h.lower(), official_header) for h in header]
     table[0] = header
 
     return table
+
+
+def crawl(url, official_header, index=0, whole_page=False):
+    started_at = time.time()
+    log.warning('### Loading table from %s ###' % url)
+
+    page = _load_page(url)
+    html_table = _find_table(page, index, whole_page)
+    table = _crawl_table(html_table)
+    table = _parse_header(table, official_header)
+
+    log.warning('### Finished in %.2f seconds ###' % (time.time() - started_at))
+    log.warning('### Total of rows %s ###' % len(table))
+
+    return pd.DataFrame(table[1:], columns=table[0])
